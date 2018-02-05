90ec6f213b75efab962defa5673c5f06424a507d
Zephyr Penoyre <zephyrpenoyre@gmail.com>
2018-02-05 16:33:30 -0500

    renamed to oot, cleaned up a lot of stuff, converted to using all parameters living in pl, added conversions to batman and stellar anchor parameters, now calculates all relative parameters (independently or all together)
    basically done but completely untested, so that seems like a good idea

---
+++ oot.py
@@ -0,0 +1,132 @@
+import numpy as np
+import scipy.optimize
+import scipy.integrate
+G=3.925e8 #in Rsun, Msun and yrs
+Rsun=6.96e8 #in m
+Msun=3e20 #in kg
+yr=3.15e7 #in seconds
+
+class planet:
+    def __init__(self,M,Mp,R,a,e,beta=8.72,Rp=0.1,Ag=0.1):
+        self.M=M
+        self.R=R
+        self.beta=beta
+        self.Mp=Mp
+        self.Rp=Rp
+        self.Ag=Ag
+        self.a=a
+        self.e=e
+        self.vTheta=vTheta
+        self.vPhi=vPhi
+
+def findT(eta,pl,t=0): #can be used to find t (when t argument omitted) or solved to find eta (when t supplied) 
+    return np.sqrt((pl.a**3)/(G*pl.M)) * (eta - pl.e*np.sin(eta)) - t #=0 when eta is the correct value (used in below root solve)
+def findEta(t,pl):
+    if pl.e==0:
+        thisEta=np.sqrt(G*pl.M/(pl.a**3))*t
+    else:
+        uBound=t*np.sqrt((G*pl.M) / (pl.a**3)) + 2*pl.e
+        lBound=t*np.sqrt((G*pl.M) / (pl.a**3)) - 2*pl.e
+        thisEta=scipy.optimize.fsolve(findT,0.5*(lBound+uBound),args=(pl,t)) #must find eta by a (well-behaved) numeric root solve
+    return thisEta
+def findPhi(t,pl):
+    eta=findEta(t,pl)
+    return 2*np.arctan(np.sqrt((1+pl.e)/(1-pl.e))*np.tan(eta/2)) % (2*np.pi)
+def findEtaPhi(Phi,pl): #may never use, but just in case we know Phi and would like to find eta (and subsequently t)
+    return 2*np.arctan(np.sqrt((1-pl.e)/(1+pl.e))*np.tan(Phi/2))
+
+def findAlpha(pl):
+    return pl.beta*(pl.Mp/pl.M)*np.power(pl.R/pl.a,3)/4
+def findGamma(t,pl):
+    eta=findEta(t,pl)
+    alpha=findAlpha(pl)
+    return alpha*np.power(1-pl.e*np.cos(eta),-3)
+def findEpsilon(t,pl,whichMode=1): #computes everything you need for epsilon in one 
+    if whichMode==1: #return the full expression (you'll almost always want this)
+        a,b,c=4,-2,-2
+    if whichMode==0: #return just m=0 mode
+        a,b,c=1,1,-2
+    if whichMode==2: #return m=2 + m=-2 contribution (these are identical, so equivalent to 2*(m=2 contribution))
+        a,b,c=(3/2),-(3/2),0
+    Phi=findPhi(t,pl)
+    psi=pl.vPhi-Phi
+    gamma=findGamma(t,pl)
+    return gamma*(a*np.power(np.sin(pl.vTheta)*np.cos(psi),2) + b*np.power(np.sin(pl.vTheta)*np.sin(psi),2) + c*np.power(np.cos(pl.vTheta),2))
+
+def findMu(t,pl):
+    eta=findEta(t,pl)
+    alpha=findAlpha(pl)
+    return alpha*pl.R*np.sqrt(G*pl.M/pl.a**3)*np.power(1-pl.e*np.cos(eta),-5)
+def findVelocity(t,pl,whichMode=1):
+    if whichMode==1: #return the full expression (you'll almost always want this)
+        a,b,c=4,-2,-2
+    if whichMode==0: #return just m=0 mode
+        a,b,c=1,1,-2
+    if whichMode==2: #return m=2 + m=-2 contribution (these are identical, so equivalent to 2*(m=2 contribution))
+        a,b,c=(3/2),-(3/2),0
+    Phi=findPhi(t,pl)
+    psi=pl.vPhi-Phi
+    mu=findMu(t,pl)
+    eta=findEta(t,pl)
+    lhs=2*np.sqrt(1-pl.e**2)*(np.sin(pl.vTheta)**2)*np.sin(2*psi)*(a-b)
+    rhs=3*pl.e*np.sin(eta)*(a*np.power(np.sin(pl.vTheta)*np.cos(psi),2) + b*np.power(np.sin(pl.vTheta)*np.sin(psi),2) + c*np.power(np.cos(pl.vTheta),2))
+    return -mu*(lhs+rhs)
+    
+def vOrbit(t,pl):
+    Phi=findPhi(t,pl)
+    psi=pl.vPhi-Phi
+    return np.sqrt(G*pl.M/(pl.a*(1-pl.e**2)))*(pl.Mp/pl.M)*np.sin(pl.vTheta)*(np.sin(psi)+pl.e*np.sin(pl.vPhi))*(Rsun/yr) # in ms-1
+def vTide(t,pl,whichMode=1):
+    velocity=findVelocity(t,pl,whichMode=whichMode)
+    return -(4/15)*velocity
+def vSum(t,pl):
+    return vOrbit(t,pl)+vTide(t,pl)
+def deltaTide(t,pl,whichMode=1):
+    epsilon=findEpsilon(t,pl,whichMode=whichMode)
+    return -(5/4)*epsilon
+def deltaBeam(t,pl):
+    velocity=vOrbit(t,pl)
+    return 4*velocity/3e8
+def deltaReflect(t,pl,Ag=0.1):
+    Phi=findPhi(t,pl)
+    psi=pl.vPhi-Phi
+    eta=findEta(t,pl)
+    cGamma=np.sin(pl.vTheta)*np.cos(psi)
+    sGamma=np.sqrt(1-cGamma**2)
+    gamma=np.abs(np.arctan2(sGamma,cGamma))
+    num=sGamma+(np.pi-gamma)*cGamma
+    denom=np.pi*(1-pl.e*np.cos(eta))
+    return Ag*np.power(pl.Rp/pl.a,2)*num/denom
+def deltaSum(t,pl):
+    return deltaTide(t,pl)+deltaBeam(t,pl)+deltaReflect(t,pl)
+
+def batman(pl): #returns parameters for batman (https://www.cfa.harvard.edu/~lkreidberg/batman/) to use, assuming planet transits
+    eta0=findEtaPhi(pl.vPhi,pl)
+    t0=365*findT(eta0,pl) #time of transit in days (between 0 and P), remembering periapse is at t=0
+    per=365*findT(2*np.pi,pl) #orbital period in days
+    rp=pl.Rp/pl.R #planet radius in units of stellar radius
+    a=pl.a/pl.R #semi-major axis in units of stellar radius
+    inc=(((180/np.pi)*pl.vTheta+90)%180)-90 #orbital inclination in degrees (same measure, except planet-people convention is -90 to 90 I believe)
+    ecc=pl.e #orbital eccentricity (exactly the same)
+    w=(90-((180/np.pi)*pl.vPhi)) % 360 #longitude of periastron in degrees
+    
+    b=pl.a*(1-pl.e*np.cos(eta0))*np.cos(pl.vTheta)/pl.R #projected impact paramter of planet at closest approach
+    if b>(pl.R+pl.Rp)/pl.R:
+        print('Planet does not transit!') #perhaps this should throw a more substantial error...
+    return t0,per,rp,a,inc,ecc,w
+    
+def anchors(pl): #returns parameters for stellar anchor calculations (https://arxiv.org/abs/1710.07293) to use, assuming planet transits
+    eta0=findEtaPhi(pl.vPhi,pl)
+    t0=365*findT(eta0,pl) #time of transit in days (between 0 and P), remembering periapse is at t=0
+    per=365*findT(2*np.pi,pl) #orbital period in days
+    b=pl.a*(1-pl.e*np.cos(eta0))*np.cos(pl.vTheta)/pl.R #projected impact paramter of planet at closest approach
+    if b>(pl.R+pl.Rp)/pl.R:
+        print('Planet does not transit!') #perhaps this should throw a more substantial error...
+    rho=3*pl.M*Msun/(4*np.pi*(pl.R*Rsun)**3) #mean stellar density in kgm-3
+    logRho=np.log10(rho)
+    Rp_R=pl.Rp/pl.R # ratio of planetary to stellar radii
+    omega=(np.pi/2)-pl.vPhi # crazy planet-people angle
+    cos=np.sqrt(pl.e)*np.cos(omega) # why would anyone want these
+    sin=np.sqrt(pl.e)*np.sin(omega) # I'm sure there's a good reason but...
+    return t0,per,b,logRho,Rp_R,cos,sin
+    
